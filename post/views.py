f296ad8b9bfb7311a3364d07cd0dfe10464660f8
Ana Maria Gomes <gomesabam@gmail.com>
2026-01-21 11:16:59 -0300

    template posts e renderização

--- post/views.py
+++ post/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
     return HttpResponse("Olá, Mundo!")
+
+def lista_post(request):
+    lista_de_posts = Post.objects.all()
+    return render(request,'posts.html',  {'lista_de_posts': lista_de_posts})
 
